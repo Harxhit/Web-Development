7be2ca959fc883054c109d0734f0547c11a388e1
Harshit Singh Parihar <harsxit04@gmail.com>
2025-02-04 22:35:00 +0530

    Files

--- Javascript/Excercise/Practice-5/script.js
+++ Javascript/Excercise/Practice-5/script.js
@@ -154,19 +154,169 @@ class Book {
     this.author = author;
     this.title = title;
   }
+  detail() {
+    return `The name of the book is ${this.title} written by ${this.author} the price of the book is $${this.price}`;
+  }
   applyDiscount(discount) {
-
+    const discountedPrice = this.price - this.price * (discount / 100);
+    return `The price of the book after a $${discount} of discount is $${discountedPrice}`;
   }
 }
+let book1 = new Book("Atomic Habits", "James Clear", 500);
+// console.log(book1.detail());
+// console.log(book1.applyDiscount(20));
 
 /* 10. Create a class `Shape` with a method `getArea()`. Extend it to create classes `Square` and `Triangle`, overriding the `getArea()` method. */
 
-/* 11. Implement a `Person` class with a method `introduce()` that logs a greeting. Then, create a `Teacher` class that extends `Person` and overrides `introduce()` with a special message. */
+class Shape {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  getArea() {
+    return `Area of something shape`;
+  }
+}
+
+class Square extends Shape {
+  getArea(side) {
+    const area = side * side;
+    return `The area of square is ${area}`;
+  }
+}
+
+class Triangle extends Shape {
+  getArea(height, base) {
+    const area = 0.5 * base * height;
+    return `The area of the triangle is ${area}`;
+  }
+}
+const square = new Square();
+const triangle = new Triangle();
+// console.log(square.getArea(2));
+// console.log(triangle.getArea(2,5));
+
+/* 11. Implement a `Student` class with a method `introduce()` that logs a greeting. Then, create a `Teacher` class that extends `Student` and overrides `introduce()` with a special message. */
+
+class Student1 {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+
+  introduce() {
+    return `Hi, my name is ${this.name}, I am ${this.age} years old, and I study in grade ${this.grade}.`;
+  }
+}
+
+class Teacher extends Student1 {
+  constructor(name, age, subject) {
+    super(name, age, "N/A"); // Teachers don't have a grade, so set it as "N/A"
+    this.subject = subject;
+  }
+
+  introduce() {
+    return `Hello, I am ${this.name}, a ${this.age}-year-old teacher, and I teach ${this.subject}.`;
+  }
+}
+
+let student1 = new Student1("Alice", 14, 9);
+// console.log(student1.introduce());
+
+let teacher1 = new Teacher("Mr. Smith", 35, "Mathematics");
+// console.log(teacher1.introduce());
 
 /* 12. Implement a class `Movie` with properties `title`, `director`, and `year`. Add a method `getDetails()` to return a string with the movie details. */
 
+class Movie {
+  constructor(title, director, year) {
+    this.title = title;
+    this.director = director;
+    this.year = year;
+  }
+  getDetails() {
+    return `The name of movie is ${this.title} it is directed by ${this.director} and released on ${this.year}`;
+  }
+}
+
+const movie = new Movie("Demolition", "Someone", 2015);
+// console.log(movie.getDetails());
+
 /* 13. Create a class `Library` that contains an array of books. Implement methods to add a book, remove a book, and display all books. */
+
+class Library {
+  constructor() {
+    this.books = []; // Array to store books
+  }
+
+  addBook(title, author) {
+    this.books.push({ title, author });
+    return `${title} by ${author} added to the library.`;
+  }
+
+  removeBook(title) {
+    let index = this.books.findIndex((book) => book.title === title);
+    if (index !== -1) {
+      this.books.splice(index, 1);
+      return `${title} has been removed from the library.`;
+    }
+    return `Book not found!`;
+  }
+
+  displayBooks() {
+    if (this.books.length === 0) {
+      return "No books available in the library.";
+    }
+    return this.books
+      .map((book) => `${book.title} by ${book.author}`)
+      .join("\n");
+  }
+}
+
+let myLibrary = new Library();
+// console.log(myLibrary.addBook("Atomic Habits", "James Clear"));
+// console.log(myLibrary.addBook("The Alchemist", "Paulo Coelho"));
+// console.log(myLibrary.displayBooks());
+// console.log(myLibrary.removeBook("Atomic Habits"));
+// console.log(myLibrary.displayBooks());
 
 /* 14. Create an `Item` class with properties `name` and `price`. Implement a method to calculate the total price after applying tax. */
 
+class Item {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  finalPrice(tax) {
+    const taxCalculation = this.price + (this.price * tax) / 100;
+    return `The final price of the ${this.name} after $${tax} is $${taxCalculation}`;
+  }
+}
+let item1 = new Item("Speaker", 500);
+// console.log(item1.finalPrice(20));
+
 /* 15. Implement a class `Counter` with methods `increment()` and `decrement()`. Add a method `getCount()` to retrieve the current count value. */
+
+class Counter {
+  constructor() {
+    this.count = 0;
+  }
+  increment() {
+    this.count++;
+    return `Current incemented count is ${this.count}`;
+  }
+  decrement() {
+    this.count--;
+    return `Current decremented count is ${this.count}`;
+  }
+  getCount() {
+    return `Current count is ${this.count}`;
+  }
+}
+
+let counter = new Counter();
+// console.log(counter.getCount()); 
+// console.log(counter.increment()); 
+// console.log(counter.increment()); 
+// console.log(counter.decrement()); 
+// console.log(counter.getCount());  
