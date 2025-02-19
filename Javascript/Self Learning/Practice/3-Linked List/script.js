b88dbe945351eb33e164946d2ea9a1cb4f6c21dc
Harshit Singh Parihar <harsxit04@gmail.com>
2025-02-19 13:22:31 +0530

    Files

--- Javascript/Self Learning/Practice/3-Linked List/script.js
+++ Javascript/Self Learning/Practice/3-Linked List/script.js
@@ -10,6 +10,48 @@ Output:
   head → [1] → [2] → [3] → null
 */
 
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+
+//   addLast(data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//       this.head = newNode;
+//       return;
+//     }
+//     let current = this.head;
+//     while (current.next) {
+//       current = current.next;
+//     }
+//     current.next = newNode;
+//   }
+
+//   printList() {
+//     let current = this.head;
+//     let output = "";
+//     while (current) {
+//       output += `[${current.data}] -> `;
+//       current = current.next;
+//     }
+//     console.log("head->", output, "null");
+//   }
+// }
+
+// const list = new LinkedList();
+// list.addLast(1);
+// list.addLast(2);
+// list.addLast(3);
+//list.printList();
+
 /* 2. Implement a Doubly Linked List */
 /*
 Input:
@@ -30,6 +72,47 @@ Input:
 Output:
   3
 */
+
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+
+//   addLast(data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//       this.head = newNode;
+//       return;
+//     }
+//     let current = this.head;
+//     while (current.next) {
+//       current = current.next;
+//     }
+//     current.next = newNode;
+//   }
+
+//   size() {
+//     let current = this.head;
+//     let count = 0;
+//     while (current) {
+//       count++;
+//       current = current.next;
+//     }
+
+//     console.log(count);
+//   }
+// }
+// const list = new LinkedList();
+// list.addLast(10);
+// list.addLast(20);
+// list.addLast(30);
+// list.size();
 
 /* 4. Search for an Element in a Linked List */
 /*
@@ -52,6 +135,51 @@ Output:
   head → [1] → [2] → [3] → null
 */
 
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+
+//   addFirst(data) {
+//     const newNode = new Node(data);
+//     newNode.next = this.head;
+//     this.head = newNode;
+//   }
+
+//   addLast(data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//       this.head = newNode;
+//       return;
+//     }
+//     let current = this.head;
+//     while (current.next) {
+//       current = current.next;
+//     }
+//     current.next = newNode;
+//   }
+//   printList() {
+//     let current = this.head;
+//     let output = "";
+//     while (current) {
+//       output += `[${current.data}] -> `;
+//       current = current.next;
+//     }
+//     console.log("head ->", output, "null");
+//   }
+// }
+// const list = new LinkedList();
+// list.addLast(2);
+// list.addLast(3);
+// list.addFirst(1);
+// list.printList();
+
 /* 6. Insert at the End of a Linked List */
 /*
 Input:
@@ -61,6 +189,46 @@ Input:
 Output:
   head → [1] → [2] → [3] → null
 */
+
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+
+//   addLast(data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//       this.head = newNode;
+//       return;
+//     }
+//     let current = this.head;
+//     while (current.next) {
+//       current = current.next;
+//     }
+//     current.next = newNode;
+//   }
+
+//   printList() {
+//     let current = this.head;
+//     let output = "";
+//     while (current) {
+//       output += `[${current.data}] -> `;
+//       current = current.next;
+//     }
+//     console.log("head ->", output, "null");
+//   }
+// }
+// const list = new LinkedList();
+// list.addLast(1);
+// list.addLast(2);
+// list.addLast(3);
+// list.printList();
 
 /* 7. Insert at a Given Position in a Linked List */
 /*
