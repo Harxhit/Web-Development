1dd2853cbfa736a311e857e0605a48dacf5c5a06
Harshit Singh Parihar <harsxit04@gmail.com>
2025-04-05 16:59:30 +0530

    Files

--- Javascript/Self Learning/7-Graph/script.js
+++ Javascript/Self Learning/7-Graph/script.js
@@ -149,3 +149,133 @@ function undirectedPath(edges, nodeA, nodeB) {
 }
 
 //console.log(undirectedPath(edges, "i", "k"));
+
+/** 
+Problem 3  : Write a function , connectedComponentsCount , that takes in the adjaceny list of an undirected graph . the function should return the number of connected components within the graph. 
+**/
+
+const graph1 = {
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2],
+};
+
+const connectedComponentsCount = (graph1) => {
+  let visited = new Set();
+  let count = 0;
+
+  for (let node in graph1) {
+    if (explore(graph1, node, visited)) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const explore = (graph1, current, visited) => {
+  if (visited.has(String(current))) return false;
+
+  visited.add(String(current));
+
+  for (let neighbour of graph1[current]) {
+    explore(graph1, neighbour, visited);
+  }
+  return true;
+};
+//console.log(connectedComponentsCount(graph1));
+
+/** 
+Problem 4 : Write a function largestComponent hat takes in the adjaency list of an undirected graph . The function should return the size of the largest component in the graph.  
+**/
+
+const largestComponent = (graph1) => {
+  let largest = 0;
+  let visited = new Set();
+
+  for (let node in graph1) {
+    const size = exploreSize(graph1, node, visited);
+    if (size > largest) largest = size;
+  }
+
+  return largest;
+};
+
+const exploreSize = (graph1, current, visited) => {
+  if (visited.has(String(current))) return 0;
+  visited.add(String(current));
+
+  let size = 1;
+  for (let neighbour of graph1[current]) {
+    size += exploreSize(graph1, neighbour, visited);
+  }
+  return size;
+};
+
+//console.log(largestComponent(graph1));
+
+/** 
+Problem 4 : Write a function, shortestPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1. You can assume that A and B exist as nodes in the graph.
+**/
+
+const edge = [
+  ["w", "x"],
+  ["x", "y"],
+  ["z", "y"],
+  ["z", "v"],
+  ["w", "v"],
+];
+
+const shortestPath = (edge, nodeA, nodeB) => {
+  const graph = convertEdge(edge);
+  let queue = [[nodeA, 0]];
+  let visited = new Set([nodeA]);
+
+  while (queue.length > 0) {
+    const [node, distance] = queue.shift();
+    if (node === nodeB) return distance;
+
+    for (let neighbour of graph[node]) {
+      if (!visited.has(neighbour)) {
+        visited.add(neighbour);
+        queue.push([neighbour, distance + 1]);
+      }
+    }
+  }
+  return -1;
+};
+
+const convertEdge = (edge) => {
+  let graph = {};
+
+  edge.forEach(([u, v]) => {
+    if (!graph[u]) graph[u] = [];
+    if (!graph[v]) graph[v] = [];
+
+    graph[u].push(v);
+    graph[v].push(u);
+  });
+  return graph;
+};
+
+//console.log(shortestPath(edge, "w", "z"));
+
+/** 
+Problem 5 : Write a function, islandCount, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the number of islands on the grid. An island is a vertically or horizontally connected region of land.
+**/
+
+const grid = [
+  ["W", "L", "W", "W", "W"],
+  ["W", "L", "W", "W", "W"],
+  ["W", "W", "W", "L", "W"],
+  ["W", "W", "L", "L", "W"],
+  ["L", "W", "W", "L", "L"],
+  ["L", "L", "W", "W", "W"],
+];
+
+const islandCount = (grid) => {};
+
+islandCount(grid); // 3
