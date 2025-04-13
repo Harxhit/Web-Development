d86d3cea8e18dceb2654d1c03c4af13dda5ed6dd
Harshit Singh Parihar <harsxit04@gmail.com>
2025-04-13 21:19:46 +0530

    Files

--- DSA/Leetcode/Easy/script.js
+++ DSA/Leetcode/Easy/script.js
@@ -1482,7 +1482,31 @@ var islandPerimeter = function (grid) {
     return result;
   }
 };
-/**/
+
+/* Question 46 : Longest Common Prefix 
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+*/
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+
+      if (prefix === "") return "";
+    }
+  }
+  if (strs.length === 0) return "";
+  if (strs.length === 1) return strs;
+  return prefix;
+};
 /**/
 /**/
 /**/
