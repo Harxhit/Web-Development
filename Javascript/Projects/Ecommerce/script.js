5fd3bfb7a61cfc76fa4f651ce319ce1a48292574
Harshit Singh Parihar <harsxit04@gmail.com>
2025-04-27 13:15:05 +0530

    Files

---
+++ Javascript/Projects/Ecommerce/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const addToCartButton1 = document.getElementById("product-1-btn");
+  const addToCartButton2 = document.getElementById("product-2-btn");
+  const addToCartButton3 = document.getElementById("product-3-btn");
+  const addToCartButton4 = document.getElementById("product-4-btn");
+  const addToCartButton5 = document.getElementById("product-5-btn");
+  const addToCartButton6 = document.getElementById("product-6-btn");
+  const product1Price = document.getElementById("product-1-price");
+  const product2Price = document.getElementById("product-2-price");
+  const product3Price = document.getElementById("product-3-price");
+  const product4Price = document.getElementById("product-4-price");
+  const product5Price = document.getElementById("product-5-price");
+  const product6Price = document.getElementById("product-6-price");
+  const product1 = document.getElementById("product-1-name");
+  const product2 = document.getElementById("product-2-name");
+  const product3 = document.getElementById("product-3-name");
+  const product4 = document.getElementById("product-4-name");
+  const product5 = document.getElementById("product-5-name");
+  const product6 = document.getElementById("product-6-name");
+
+  let total = 0;
+});
